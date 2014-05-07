b6dfb3e395fe7b6d4c404327188c51ec45528b8f
catal_000 <catal_000@192.168.1.111>
2014-05-07 13:33:18 -0700

    AdditionProject

---
+++ additionProject/src/additionProject.java
@@ -0,0 +1,33 @@
+		
+import java.util.Scanner;
+
+public class additionProject {
+
+	public static void main(String[] args) 
+	{
+		
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(system.in);
+		
+		int num1;
+		int num2;
+		int sum;
+		
+		System.out.print("enter first number");
+		num1 = input.nextInt();
+		
+		System.out.print("enter second number");
+		num2 = input.nextInt();
+		
+		sum = num1 + num2;
+		
+		System.out.printf("sum is %d\n", sum);
+	}
+
+}
+
+
+
+		
+		
+
